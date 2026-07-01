eab5f454ba47e6d753a2d6729f4aec190c8a552c
Thomas Owen <thomas.owen@ons.gov.uk>
2026-07-01 12:07:15 +0100

    chore: move classifai and EmbeddingHandler to top level of package

--- src/survey_assist_embed_core/__init__.py
+++ src/survey_assist_embed_core/__init__.py
@@ -1,1 +1,13 @@
 """Shared retrieval primitives for Survey Assist embedding and search."""
+
+from survey_assist_embed_core.adapters.classifai.vector_backend import (
+    ClassifaiVectorBackend,
+    build_classifai_vector_store_artifacts,
+)
+from survey_assist_embed_core.embed.embedding import EmbeddingHandler
+
+__all__ = [
+    "ClassifaiVectorBackend",
+    "EmbeddingHandler",
+    "build_classifai_vector_store_artifacts",
+]
